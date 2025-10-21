03c75fbc3c259c181858f6dfe3490b99105b8735
Sonase1 <sondreasebo@gmail.com>
2025-10-21 10:18:47 +0200

    Update Customer.java

--- src/main/java/no/hvl/dat100/javel/oppgave3/Customer.java
+++ src/main/java/no/hvl/dat100/javel/oppgave3/Customer.java
@@ -2,14 +2,14 @@ package no.hvl.dat100.javel.oppgave3;
 
 public class Customer {
 
-    // TODO - object variables (attributes)
+    
 
     public Customer(String name, String email, int customer_id, PowerAgreementType agreement) {
 
         // TODO
     }
 
-    // TODO - getter/setter methods for all objectvariables
+    // TODO - getter/setter methods for all object variables
 
     // TODO - toString method
 
